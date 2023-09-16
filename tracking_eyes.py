27899dd8adb51ff47ae6a2dec56972d5ec3220bc
Heider Rivas <heiderarb@gmail.com>
2023-09-16 15:10:48 -0500

    The test file is a face counter. the eye tracker has been finished

--- tracking_eyes.py
+++ tracking_eyes.py
@@ -2,13 +2,11 @@ import cv2
 import dlib
 import numpy as np
 
-
 def shape_numpy(shape, dtype="int"):
     coordinates = np.zeros((68, 2), dtype=dtype)
     for i in range(0, 68):
         coordinates[i] = (shape.part(i).x, shape.part(i).y)
     return coordinates
-
 
 def eye_on_mask(mask, side):
     points = [shape[i] for i in side]
@@ -16,53 +14,56 @@ def eye_on_mask(mask, side):
     mask = cv2.fillConvexPoly(mask, points, (0, 255, 0))
     return mask
 
-
-def count(tresh, mid, img, right=False):
-    cs, _ = cv2.findContours(tresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    try:
-        c = max(cs, key= cv2.contourArea)
-        M = cv2.moments(c)
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-        if right:
-            cx += mid
-        cv2.circle(img, (cx,cy), 4 , (0, 255, 0), 2)
-    except:
-        pass
-
+def find_eyes_center(shape, side):
+    points = [shape[i] for i in side]
+    x = sum([p[0] for p in points]) / len(points)
+    y = sum([p[1] for p in points]) / len(points)
+    return int(x), int(y)
 
 detector = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor('shape_68.dat')
+predictor = dlib.shape_predictor('shape_68.dat')
 
 left = [36, 37, 38, 39, 40, 41]
 right = [42, 43, 44, 45, 46, 47]
 
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
-tresh = img.copy()
+thresh = img.copy()
 
 cv2.namedWindow('image')
 kernel = np.ones((9, 9), np.uint8)
 
-
 def nothing(x):
     pass
 
+cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
 
-cv2.createTrackbar('treshold', 'image', 0, 255, nothing)
-
-while (True):
+while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rects = detector(gray, 1)
     for rect in rects:
-        shape = predict(gray, rect)
+        shape = predictor(gray, rect)
         shape = shape_numpy(shape)
         mask = np.zeros(img.shape[:2], dtype=np.uint8)
         mask = eye_on_mask(mask, left)
         mask = eye_on_mask(mask, right)
-        mask = cv2.dilate(mask, kernel,5)
+        mask = cv2.dilate(mask, kernel, 5)
         eyes = cv2.bitwise_and(img, img, mask=mask)
-        mask = (eyes == [0, 0, 0].all(axis=2))
+        mask = (eyes == [0, 0, 0]).all(axis=2)
         eyes[mask] = [255, 255, 255]
+
+        left_eye_center = find_eyes_center(shape, left)
+        right_eye_center = find_eyes_center(shape, right)
+
+        cv2.circle(img, left_eye_center, 10, (0, 255, 0), -1)
+        cv2.circle(img, right_eye_center, 10, (0, 255, 0), -1)
+
+    cv2.imshow('eyes', img)
+    cv2.imshow('image', thresh)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
